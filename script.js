6c1801456d0ceb47ee657dddb085f276e8a56d56
snapekk <126613007+snapekk@users.noreply.github.com>
2023-02-28 17:34:56 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Cria o evento de clique para as caixas
-var boxes = document.getElementsByClassName('box');
-for (var i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener('click', function() {
-		window.location.href = 'https://www.youtube.com';
-	});
-}
